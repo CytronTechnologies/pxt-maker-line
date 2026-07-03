3b0e0264b26cb6f2e7f576a51f6f542143ba613e
noqman <140384051+noqman@users.noreply.github.com>
2026-07-03 12:02:26 +0800

    Implement tests for calibration and line detection
    Added tests for calibration and line detection functionality.

--- test.ts
+++ test.ts
@@ -1,2 +1,36 @@
-// tests go here; this will not be compiled when this package is used as an extension.
-
+/**
+ * tests go here; this will not be compiled when this package is used as a library
+ */
+input.onButtonPressed(Button.A, function () {
+    // Enter calibration mode.
+    makerline.enterCalibration()
+})
+input.onButtonPressed(Button.B, function () {
+    // Save calibration data and exit calibration mode.
+    makerline.exitCalibration()
+})
+makerline.initializeCalibration(DigitalPin.P2)
+makerline.initializeDigital(
+DigitalPin.P12,
+DigitalPin.P13,
+DigitalPin.P14,
+DigitalPin.P15,
+DigitalPin.P16
+)
+// Since this is the last initialization block, analog mode will be used.
+makerline.initializeAnalog(AnalogReadWritePin.P1)
+basic.forever(function () {
+    if (makerline.isLineDetectedOn(makerline.LinePosition.FarLeft)) {
+        serial.writeLine("Far Left")
+    } else if (makerline.isLineDetectedOn(makerline.LinePosition.Left)) {
+        serial.writeLine("Left")
+    } else if (makerline.isLineDetectedOn(makerline.LinePosition.Center)) {
+        serial.writeLine("Center")
+    } else if (makerline.isLineDetectedOn(makerline.LinePosition.Right)) {
+        serial.writeLine("Right")
+    } else if (makerline.isLineDetectedOn(makerline.LinePosition.FarRight)) {
+        serial.writeLine("Far Right")
+    } else {
+        serial.writeLine("No line detected")
+    }
+})
